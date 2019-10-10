68e75dabe5f545415b5089ceb8f7acf1319a2e6a
hmbashar <hmbashar@gmail.com>
2019-10-11 00:08:18 +0600

    home markup done

--- inc/template/proexpert.php
+++ inc/template/proexpert.php
@@ -2,9 +2,185 @@
 /*
 Template Name: Pro Expert Finder
 */
-get_header();
+get_header();  ?>
 
-?>
+<section class="pro-expert-finder-area">
+	<div class="container">
+		<!-- pro expert finder form -->
+		<div class="row">
+			<div class="col-xl-12">
+				<div class="pro-expert-finder-form">
+					<form action="">
+						<input type="text" placeholder="Your Name" name="" id="">
+						<input type="submit" value="submit">
+					</form>
+				</div>
+			</div>
+		</div>
+		<!--/ pro expert finder form -->
+		<div class="pro-expert-finder-photographer">
+			<div class="pro-expert-finder-title">
+				<h2>Select Your Photographer</h2>
+			</div>
+			<div class="row">
+				<!-- single photographer finder -->
+				<div class="col-xl-4 col-md-4">
+					<div class="single-pro-expert-photographer">
+						<div class="pro-expert-photographer-full-thumb">
+							<div class="pro-expert-photographer-cover-photo">
+								<img src="<?php echo WP_PRO_PLUGIN_DIR ; ?>/assets/images/pro-expert-single-img01.jpg" alt="">
+							</div>
+							<div class="pro-expert-photographer-client">
+								<div class="row">
+									<div class="col-xl-4">
+										<div class="pro-expert-photographer-category">
+											<p>Los Angeles</p>
+										</div>
+									</div>
+									<div class="col-xl-4">
+										<div class="pro-expert-photographer-img">
+											<img src="<?php echo WP_PRO_PLUGIN_DIR ; ?>/assets/images/team-photo-3.jpg" alt="">
+										</div>
+									</div>
+									<div class="col-xl-4">
+										<div class="pro-expert-photographer-review">
+
+											<i class="fas fa-star"></i>
+											<i class="fas fa-star"></i>
+											<i class="fas fa-star"></i>
+											<i class="fas fa-star"></i>
+											<!-- <i class="fas fa-star"></i> -->
+
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="single-pro-expert-social">
+								<a href=""><i class="fab fa-facebook-f"></i></a>
+								<a href=""><i class="fab fa-twitter"></i></a>
+								<a href=""><i class="fab fa-linkedin-in"></i></a>
+								<a href=""><i class="fab fa-instagram"></i></a>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xl-12">
+								<div class="pro-expert-finder-author-title">
+									<a href=""><h2>Voluptatum unde minus</h2></a>
+									<a href=""> View Portfolio </a>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<!--/ single photographer finder -->
+				<!-- single photographer finder -->
+				<div class="col-xl-4 col-md-4">
+					<div class="single-pro-expert-photographer">
+						<div class="pro-expert-photographer-full-thumb">
+							<div class="pro-expert-photographer-cover-photo">
+								<img src="<?php echo WP_PRO_PLUGIN_DIR ; ?>/assets/images/pro-expert-single-img01.jpg" alt="">
+							</div>
+							<div class="pro-expert-photographer-client">
+								<div class="row">
+									<div class="col-xl-4">
+										<div class="pro-expert-photographer-category">
+											<p>Los Angeles</p>
+										</div>
+									</div>
+									<div class="col-xl-4">
+										<div class="pro-expert-photographer-img">
+											<img src="<?php echo WP_PRO_PLUGIN_DIR ; ?>/assets/images/team-photo-3.jpg" alt="">
+										</div>
+									</div>
+									<div class="col-xl-4">
+										<div class="pro-expert-photographer-review">
+
+											<i class="fas fa-star"></i>
+											<i class="fas fa-star"></i>
+											<i class="fas fa-star"></i>
+											<i class="fas fa-star"></i>
+											<!-- <i class="fas fa-star"></i> -->
+
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="single-pro-expert-social">
+								<a href=""><i class="fab fa-facebook-f"></i></a>
+								<a href=""><i class="fab fa-twitter"></i></a>
+								<a href=""><i class="fab fa-linkedin-in"></i></a>
+								<a href=""><i class="fab fa-instagram"></i></a>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xl-12">
+								<div class="pro-expert-finder-author-title">
+									<a href=""><h2>Voluptatum unde minus</h2></a>
+									<a href=""> View Portfolio </a>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<!--/ single photographer finder -->
+				<!-- single photographer finder -->
+				<div class="col-xl-4 col-md-4">
+					<div class="single-pro-expert-photographer">
+						<div class="pro-expert-photographer-full-thumb">
+							<div class="pro-expert-photographer-cover-photo">
+								<img src="<?php echo WP_PRO_PLUGIN_DIR ; ?>/assets/images/pro-expert-single-img01.jpg" alt="">
+							</div>
+							<div class="pro-expert-photographer-client">
+								<div class="row">
+									<div class="col-xl-4">
+										<div class="pro-expert-photographer-category">
+											<p>Los Angeles</p>
+										</div>
+									</div>
+									<div class="col-xl-4">
+										<div class="pro-expert-photographer-img">
+											<img src="<?php echo WP_PRO_PLUGIN_DIR ; ?>/assets/images/team-photo-3.jpg" alt="">
+										</div>
+									</div>
+									<div class="col-xl-4">
+										<div class="pro-expert-photographer-review">
+
+											<i class="fas fa-star"></i>
+											<i class="fas fa-star"></i>
+											<i class="fas fa-star"></i>
+											<i class="fas fa-star"></i>
+											<!-- <i class="fas fa-star"></i> -->
+
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="single-pro-expert-social">
+								<a href=""><i class="fab fa-facebook-f"></i></a>
+								<a href=""><i class="fab fa-twitter"></i></a>
+								<a href=""><i class="fab fa-linkedin-in"></i></a>
+								<a href=""><i class="fab fa-instagram"></i></a>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xl-12">
+								<div class="pro-expert-finder-author-title">
+									<a href=""><h2>Voluptatum unde minus</h2></a>
+									<a href=""> View Portfolio </a>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<!--/ single photographer finder -->
+
+			</div>
+		</div>
+	</div>
+</section>
 
 
 
